dd3beb301f24a189ec11db5a1c28980563a96c26
Mario Visic <mario@mariovisic.com>
2011-03-31 18:58:28 +0800

    Replaced calles to the depricatd constant RAILS_ENV with Rails.env

--- lib/apn_on_rails/apn_on_rails.rb
+++ lib/apn_on_rails/apn_on_rails.rb
@@ -2,31 +2,22 @@ require 'socket'
 require 'openssl'
 require 'configatron'
 
-rails_root = File.join(FileUtils.pwd, 'rails_root')
-if defined?(RAILS_ROOT)
-  rails_root = RAILS_ROOT
-end
-
-rails_env = 'development'
-if defined?(RAILS_ENV)
-  rails_env = RAILS_ENV
-end
-
 configatron.apn.set_default(:passphrase, '')
 configatron.apn.set_default(:port, 2195)
 
 configatron.apn.feedback.set_default(:passphrase, configatron.apn.passphrase)
 configatron.apn.feedback.set_default(:port, 2196)
 
-if rails_env == 'production'
+if Rails.env.production?
   configatron.apn.set_default(:host, 'gateway.push.apple.com')
-  configatron.apn.set_default(:cert, File.join(rails_root, 'config', 'apple_push_notification_production.pem'))
+  configatron.apn.set_default(:cert, File.join(RAILS_ROOT, 'config', 'apple_push_notification_production.pem'))
   
   configatron.apn.feedback.set_default(:host, 'feedback.push.apple.com')
   configatron.apn.feedback.set_default(:cert, configatron.apn.cert)
 else
+
   configatron.apn.set_default(:host, 'gateway.sandbox.push.apple.com')
-  configatron.apn.set_default(:cert, File.join(rails_root, 'config', 'apple_push_notification_development.pem'))
+  configatron.apn.set_default(:cert, File.join(RAILS_ROOT, 'config', 'apple_push_notification_development.pem'))
   
   configatron.apn.feedback.set_default(:host, 'feedback.sandbox.push.apple.com')
   configatron.apn.feedback.set_default(:cert, configatron.apn.cert)
